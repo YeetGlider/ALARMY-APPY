de0feade1c1463f6cb42f42dc3f116fc23c5773f
YeetGlider <72340148+YeetGlider@users.noreply.github.com>
2022-04-24 11:53:30 +0530

    Add files via upload

---
+++ Screens/SoundScreen.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Header,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  PlatForm,
+  StatusBar,
+  ImageBackground,
+  Image,
+  SafeAreaView,
+} from 'react-native';
+
+import{Audio} from 'expo-av'; 
+
+
+class SoundButton extends React.Component {
+    playSound = async() =>{
+        await Audio.sound.createAsync(
+            {uri:'https://soundcloud.com/r-a-o-759929458/boywithuke-girl-i-hope-you'},
+            {uri:'https://soundcloud.com/boywithuke/blurry-nights'},
+            {uri:'https://soundcloud.com/boywithuke/two-moons'},
+            {shouldPlay: true }
+        );
+    }
+    render() {
+      return (
+
+        <TouchableOpacity
+      
+        style={[styles.buttons, {backgroundColor:this.props.color}]}
+        onPress={this.playSound}> 
+        <Text style={ styles.buttonText}> SOUND </Text>
+        </TouchableOpacity>
+     
+            
+      );
+    }}
+
+
+    const styles = StyleSheet.create({
+        button: {
+          marginTop: 100,
+          marginLeft: 80,
+          borderWidth: 1,
+          borderColor: 'rgba(0,0,0,0.2)',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: 200,
+          height: 200,
+          backgroundColor: 'red',
+          borderRadius: 100,
+        },
+        buttonText: {
+          fontWeight: 'bold',
+          fontSize: 20,
+        }
+      });
+      
+      export default SoundButton;
